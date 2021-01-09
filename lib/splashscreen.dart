527102e3d3a2966902587019ea95d881cbda45c8
fatima796 <74816845+fatima796@users.noreply.github.com>
2021-01-09 14:36:46 +0500

    splashscreen.dart

---
+++ lib/splashscreen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 30,
+        navigateAfterSeconds: new AfterSplash(),
+        title: new Text('Fatima Sohail(FA17-BSE-009)',
+          style: new TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0
+          ),),
+        image: new Image.network('https://i.pinimg.com/originals/00/52/6f/00526f8a4aada0fd55a6664993432e2a.jpg'),
+        backgroundColor: Colors.purpleAccent,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: ()=>print("Flutter Egypt"),
+        loaderColor: Colors.red
+    );
+  }
+}
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text("Welcome to splashscreen package"),
+          automaticallyImplyLeading: false
+      ),
+      body: new Center(
+        child: new Text("Done!",
+          style: new TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0
+          ),
+        ),
+
+      ),
+    );
+  }
+}
